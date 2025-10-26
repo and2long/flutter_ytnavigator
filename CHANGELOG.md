2f6303bd04aa54fa869fc503df9e71c95e75c725
Lilong Zhang <and2long@gmail.com>
2025-10-26 08:57:08 +0800

    release 0.0.7

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 0.0.7
+- 适配 fluter3.35
+
 ## 0.0.6
 - Rename NavigatorObserver to YTNavigatorObserver
 
--- lib/src/navigator_util.dart
+++ lib/src/navigator_util.dart
@@ -1,6 +1,5 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:page_transition/page_transition.dart';
-import 'package:swipeable_page_route/swipeable_page_route.dart';
 
 class NavigatorUtil {
   static Future<dynamic> push(BuildContext context, Widget child) {
@@ -14,65 +13,39 @@ class NavigatorUtil {
   static Future<dynamic> _pushRightToLeftWithFade(
     BuildContext context, {
     required Widget child,
-    bool onlySwipeFromEdge = true,
   }) {
     return _pushWithCustomFade(
       context,
       child: child,
-      onlySwipeFromEdge: onlySwipeFromEdge,
     );
   }
 
   static Future<dynamic> _pushRightToLeftWithFadeReplace(
     BuildContext context, {
     required Widget child,
-    bool onlySwipeFromEdge = true,
   }) {
     return _pushWithCustomFadeReplace(
       context,
       child: child,
-      onlySwipeFromEdge: onlySwipeFromEdge,
     );
   }
 
-  static Future<dynamic> pushBottomToTop(BuildContext context, Widget child) {
+  static Future<dynamic> _pushWithCustomFade(BuildContext context,
+      {required Widget child}) {
     return Navigator.push(
       context,
-      PageTransition(
-        type: PageTransitionType.bottomToTop,
-        alignment: Alignment.topCenter,
-        child: child,
-        settings: RouteSettings(name: child.runtimeType.toString()),
-        duration: const Duration(milliseconds: 400),
-        reverseDuration: const Duration(milliseconds: 400),
-      ),
-    );
-  }
-
-  static Future<dynamic> _pushWithCustomFade(
-    BuildContext context, {
-    required Widget child,
-    bool onlySwipeFromEdge = false,
-  }) {
-    return Navigator.push(
-      context,
-      SwipeablePageRoute(
-        canOnlySwipeFromEdge: onlySwipeFromEdge,
+      CupertinoPageRoute(
         builder: (context) => child,
         settings: RouteSettings(name: child.runtimeType.toString()),
       ),
     );
   }
 
-  static Future<dynamic> _pushWithCustomFadeReplace(
-    BuildContext context, {
-    required Widget child,
-    bool onlySwipeFromEdge = false,
-  }) {
+  static Future<dynamic> _pushWithCustomFadeReplace(BuildContext context,
+      {required Widget child}) {
     return Navigator.pushReplacement(
       context,
-      SwipeablePageRoute(
-        canOnlySwipeFromEdge: onlySwipeFromEdge,
+      CupertinoPageRoute(
         builder: (context) => child,
         settings: RouteSettings(name: child.runtimeType.toString()),
       ),
